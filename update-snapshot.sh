7fa9ed27d0b8e154103c2c7557feac8145a3c5ee
Hank Barta <hbarta@gmail.com>
2018-03-22 13:40:32 -0500

    up to date version

--- update-snapshot.sh
+++ update-snapshot.sh
@@ -15,6 +15,16 @@
 # the script will wait until it finishes. (A 'sanitized' variant of the 
 # filesystem name is used because it usually includes '/')
 
+# Much of the processing is demanding on either disk and/or CPU resources. To
+# provide better performance, these sections are interlocked such that if mutiple
+# copies of the script are launched, only one will execute the code in the
+# interlocked section. Some sections which use specific resources or remote
+# resources can proceed in parallel. Interlocks are named
+#
+# collect - 'send' incremental snapshot to a disk file (disk and CPU)
+# transmit - deliver the resulting file to the remote system (network bandwidth)
+# receive - 'receive' incremental snapshot on remote system. (remote disk,CPU)
+
 # useful functions
 
 # check to see if we can ssh to remote system. (May not respond to 'ping')
@@ -22,18 +32,6 @@ isRemoteReachable()
 {
     ssh $1 exit
     return $?
-}
-
-# write some status to /tmp
-recordStatus() # (status, host, sanitized_filesystem)
-{
-    echo $1 $$ >/tmp/update-snapshot.sh.${2}.${3}
-}
-
-# remove the status file
-removeStatus() # (host, sanitized_filesystem)
-{
-    rm "/tmp/update-snapshot.sh.${1}.${2}"
 }
 
 # get the length of a local file
@@ -49,13 +47,18 @@ fileLen()
     echo LEN
 }
 
+# external code
+
+. `dirname $0`/lock.sh || exit 1
+
 echo "########################################################################"
-date
+date +%Y-%m-%d\ %H:%M:%S
 
 # check for command line arguments
 if [ $# -lt 2 -o $# -gt 3 ]
 then
     echo "Usage $0 host filesystem [remote_filesystem]"
+    echo "v0.2"
     exit 1
 fi
 
@@ -74,26 +77,6 @@ fi
 FILESYSTEM_F=`echo $FILESYSTEM|tr / -`
 REMOTE_FILESYSTEM_F=`echo $REMOTE_FILESYSTEM|tr / -`
 
-# check to see if previous instance is still running
-# TODO: Develop a strategy not subject to race conditions
-echo checking for $PID_FILE
-PID_FILE=/tmp/update-snapshot.sh.${REMOTE_HOST}.${FILESYSTEM_F}
-while [ -e /tmp/update-snapshot.sh.${REMOTE_HOST}.${FILESYSTEM_F} ]
-do
-    PREV_PID=`< $PID_FILE awk '{print $2}'`
-    echo PREV_PID $PREV_PID
-    if ps -p $PREV_PID >/dev/null
-    then
-        echo $PREV_PID still running
-        sleep 60
-    else
-        PREV_STATUS=`< $PID_FILE awk '{print $1}'`
-        echo $PREV_PID exited abnormally after `stat -c "%y" $PID_FILE`
-        removeStatus $REMOTE_HOST $FILESYSTEM_F
-    fi
-done
-
-recordStatus "starting" $REMOTE_HOST $FILESYSTEM_F
 
 # check to see if we can reach the remote
 while ( ! isRemoteReachable $REMOTE_HOST)
@@ -106,17 +89,19 @@ done
 # TODO: cache results of 'zfs list' to use to ID the most recent snapshot
 echo
 echo "On " `hostname`
-/sbin/zfs list -t snap -r $FILESYSTEM
+/sbin/zfs list -d 1 -t snap -r $FILESYSTEM
 echo
 echo "On $REMOTE_HOST"
-ssh $REMOTE_HOST /sbin/zfs list -t snap -r $REMOTE_FILESYSTEM
+ssh $REMOTE_HOST /sbin/zfs list -d 1 -t snap -r $REMOTE_FILESYSTEM
 
 # capture newest local and remote snapshots. The date tag is separated 
 # from the filesystem part since local and remote filesystems may differ
-export REMOTE=`ssh $REMOTE_HOST /sbin/zfs list -t snap -r $REMOTE_FILESYSTEM |\
-       tail -1 | awk '{match($1,"@(.*)",a)}END{print a[1]}'`
-export LOCAL=`/sbin/zfs list -t snap -r $FILESYSTEM | \
-       tail -1 | awk '{match($1,"@(.*)",a)}END{print a[1]}'`
+export REMOTE=`ssh $REMOTE_HOST /sbin/zfs list -d 1 -t snap -r $REMOTE_FILESYSTEM |\
+       tail -1 | \
+       awk '{match($1,"@(.*)")}END{print substr($1, RSTART+1, RLENGTH)}'`
+export LOCAL=`/sbin/zfs list -d 1 -t snap -r $FILESYSTEM | \
+       tail -1 | \
+       awk '{match($1,"@(.*)")}END{print substr($1, RSTART+1, RLENGTH)}'`
 
 echo REMOTE $REMOTE
 echo LOCAL $LOCAL
@@ -132,14 +117,24 @@ then
 else
     echo
     echo snapshotting $FILESYSTEM
+    echo /usr/bin/time -p /sbin/zfs snap ${FILESYSTEM}@`date +%Y-%m-%d`
     /usr/bin/time -p /sbin/zfs snap ${FILESYSTEM}@`date +%Y-%m-%d`
-    # TODO check status of 'zfs snap'
-    export LOCAL=`/sbin/zfs list -t snap -r $FILESYSTEM | \
-        tail -1 | awk '{match($1,"@(.*)",a)}END{print a[1]}'`
+
+    PREV_LOCAL=$LOCAL
+    LOCAL=`/sbin/zfs list -d 1 -t snap -r $FILESYSTEM | tail -1 | \
+        awk '{match($1,"@(.*)")}END{print substr($1, RSTART+1, RLENGTH)}'`
+    
+    # check to see if the snapshot operation worked, $LOCAL should change
+    echo  test "$PREV_LOCAL = $LOCAL"
+    if [ $PREV_LOCAL = $LOCAL  ]
+    then
+        echo snapshot failed
+        exit 1 # no obvious recoveryzpool create
+    fi
     echo
 fi
 
-date
+date +%Y-%m-%d\ %H:%M:%S
 
 # see if we need to send
 # TODO: check if file is there, not if it is already imported
@@ -153,31 +148,59 @@ else
     LOCAL_F=${FILESYSTEM_F}@$LOCAL
     echo saving incremental remote: $REMOTE local: $LOCAL
     echo to /snapshots/${REMOTE_F}-${LOCAL_F}.snap.xz
+
+    # wait for up to 5 hours for this stage
+    if ! acquireLock "collecting" 300
+    then
+        echo $$ cannot lock "collecting"
+        exit 1
+    fi
+
     echo time -p /sbin/zfs send -L -i ${FILESYSTEM}@${REMOTE} ${FILESYSTEM}@${LOCAL}\
          \| pxz -3 -c - \(direct to /snapshots/${REMOTE_F}-${LOCAL_F}.snap.xz \)
     time -p /sbin/zfs send -L -i ${FILESYSTEM}@${REMOTE} ${FILESYSTEM}@${LOCAL}\
          | pxz -3 -c - \
          >/snapshots/${REMOTE_F}-${LOCAL_F}.snap.xz
+
+    releaseLock "collecting"
+
+    # wait for up to 5 hours for this stage
+    if ! acquireLock "transmit" 300
+    then
+        echo $$ cannot lock "transmit"
+        exit 1
+    fi
+
     echo rsync to $REMOTE_HOST
     cd /snapshots
     time -p rsync -av --partial --append-verify --progress \
 	${REMOTE_F}-${LOCAL_F}.snap.xz ${REMOTE_HOST}:/snapshots/
+    releaseLock "transmit"
 fi
 
-date
+date +%Y-%m-%d\ %H:%M:%S
 
 # now 'receive' the snapshot at the remote
 # time ssh cashapona 'xzcat /snapshots/pool2TB4K-test@initial-tank@2018-03-08.snap.xz|\
 #      zfs receive pool2TB4K/test'
-echo time ssh $REMOTE_HOST "xzcat /snapshots/${REMOTE_F}-${LOCAL_F}.snap.xz \| \
+# wait for up to 5 hours for this stage
+if ! acquireLock "receive" 300
+then
+    echo $$ cannot lock "receive"
+    exit 1
+fi
+
+echo time -p ssh $REMOTE_HOST "xzcat /snapshots/${REMOTE_F}-${LOCAL_F}.snap.xz \| \
          zfs receive $REMOTE_FILESYSTEM" 
-time ssh $REMOTE_HOST "xzcat /snapshots/${REMOTE_F}-${LOCAL_F}.snap.xz | \
+time -p ssh $REMOTE_HOST "xzcat /snapshots/${REMOTE_F}-${LOCAL_F}.snap.xz | \
          zfs receive $REMOTE_FILESYSTEM" 
+
+releaseLock "receive"
 
 # report snapshots following send
 echo
 echo "after send/receive to ${REMOTE_HOST}"
-ssh $REMOTE_HOST /sbin/zfs list -t snap -r $REMOTE_FILESYSTEM
+ssh $REMOTE_HOST /sbin/zfs list -d 1 -t snap -r $REMOTE_FILESYSTEM
 
-date
+date +%Y-%m-%d\ %H:%M:%S
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
